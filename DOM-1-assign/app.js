7f636f53786da7ebd006e61b12e85ff856bb4891
Anjali Yadav <anjaliruby790@gmail.com>
2022-08-25 16:20:21 +0530

    assignment

---
+++ DOM-1-assign/app.js
@@ -0,0 +1,33 @@
+document.querySelectorAll('div')[0].innerText = 'Anjali'
+
+document.querySelectorAll('div')[1].innerText = 'My name is Anjali. I am from Uttar Pradesh. I had completed my schooling from Kendriya Vidyalaya Shillong and Gorakhpur. I have done my B.tech in EEE from KEC, Ghaziabad.';
+
+document.querySelectorAll('div')[2].style.color='red';
+
+document.querySelectorAll('.divs')[0].style.border = 'thin solid #0000FF';
+document.querySelectorAll('.divs')[1].style.border = 'thin solid #0000FF';
+document.querySelectorAll('.divs')[2].style.border = 'thin solid #0000FF';
+
+document.getElementById('id1').append(` ${'AirCampus'}`);
+
+document.getElementsByTagName('div')[7].innerText = "This is my first assignment";
+
+document.querySelectorAll('div')[8].style.fontSize = '32px';
+document.querySelectorAll('div')[8].innerText = "My Experience with AirCampus";
+document.querySelectorAll('div')[8].style.color = 'aqua';
+document.querySelectorAll('div')[8].style.textDecoration = 'underline';
+document.querySelectorAll('div')[8].style.fontWeight = 'bold';
+
+
+
+
+document.getElementsByTagName('p')[0].innerText = "My experience with AirCampus is great. Their teachers and teaching methods is very good. Also their is 1 on 1 mentoring session and doubt session, that is very useful for each students so you can easily ask your questions and clear your doubts.";
+
+
+
+
+// let p= document.querySelectorAll('.divs');
+// for(let i=0; i<p.length; i++){
+    // p.style.border = 'thin solid #0000FF';
+// }
+
